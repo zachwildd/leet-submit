af297abb5729ba3ae8de8fc53fda10c9d496db74
Zach Wild <zacharyjwild@gmail.com>
2020-02-15 21:20:55 -0500

    client: fix submit script

--- client/public/problems/submit.js
+++ client/public/problems/submit.js
@@ -3,13 +3,13 @@ var test = document.getElementById("test-code");
 var submitButton = document.getElementById("submit-button");
 
 var url = "http://localhost:9001";
-var route = "/api/problem/submit";
+var route = "/problem/submit";
 var xhr = new XMLHttpRequest();
 
 var submit = function() {
   xhr.open("POST", url+route, true);
   // redirect to notes after posted
-  xhr.onreadystatechange = notesaved;
+  // xhr.onreadystatechange = do nothing;
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({
     src: src.value,
