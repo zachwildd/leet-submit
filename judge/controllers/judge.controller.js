cf24f7d6ae11982880b9df8a56a6269f8405071a
Zach Wild <zacharyjwild@gmail.com>
2020-02-16 01:12:31 -0500

    add judge logic for running tests

--- judge/controllers/judge.controller.js
+++ judge/controllers/judge.controller.js
@@ -3,32 +3,56 @@ const fs = require('fs');
 const judgeController = {}
 
 judgeController.post = async (req, res) => {
-  try {
-    if (req.body === null || req.body.src === null || req.body.test === null) {
-      console.log(JSON.stringify(req));
-      res.status('400');
-      res.send('src or test cannot be null');
-    }
-    // parse request body
-    const src = req.body.src;
-    const test = req.body.test;
+    console.log('Received request to judge submission');
 
-    // create js files for src and test code    
-    let thisDir = process.cwd();
-    fs.writeFileSync(thisDir + '/code/src.js', src);
-    fs.writeFileSync(thisDir + '/code/test.js', test);
+    let response = '';
+    let responseCode = '';
 
-    // run mocha tests
-    res.send('congrats');
+    try {
+      // check for boundary conditions
+      if (req.body === null || req.body.src === null || req.body.test === null) {
+        console.log('Badly formatted request body: ' + req.body);
+        res.status('400').send();
+      }
+
+      // parse request body
+      const src = req.body.src;
+      const test = req.body.test;
+
+      // create js files for src and test code    
+      let thisDir = process.cwd();
+      fs.writeFileSync(thisDir + '/code/src.js', src);
+      fs.writeFileSync(thisDir + '/code/test.js', test);
+
+      // run mocha tests
+      const { exec } = require("child_process");
+      await exec("node code/src.js", (error, stdout, stderr) => {
+        // cleanup files
+        cleanupFiles();
+        if (error) {
+          res.send(error);
+        }
+        if (stderr) {
+          res.send(stderr);
+        }
+        // send test results
+        res.send(stdout);
+      });
   }
   catch (err) {
-    console.log(err);
-    res.status('400');
-    res.send('error handling /judge post');
+      console.log(err);
+      // cleanup files
+      cleanupFiles();
+      // send error response
+      res.status('400').send();
   }
-  finally {
-    // cleanup test and src files
-  }
+}
+
+function cleanupFiles() {
+  // cleanup test and src files
+  let thisDir = process.cwd();
+  fs.truncate(thisDir + '/code/src.js', 0, function(){console.log('Cleaned up src.js')});
+  fs.truncate(thisDir + '/code/test.js', 0, function(){console.log('Cleaned up test.js')});
 }
 
 module.exports = judgeController
--- judge/routes/judge.routes.js
+++ judge/routes/judge.routes.js
@@ -1,11 +1,12 @@
-const Router = require('express').Router
+const Router = require('express').Router;
 const judgeController = require('../controllers/judge.controller')
 
 const router = new Router()
 
 router.post('/', (req, res) => {
   judgeController.post(req, res)
-    .catch( err => {
+    .catch( (err) => {
+      res.status(500).send();
     })
 });
 
