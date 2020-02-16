51c836cf1a147669cc3e89dc305afd0f7449cdb1
Zach Wild <zacharyjwild@gmail.com>
2020-02-15 21:20:40 -0500

    server: add cors add problems route

--- api/server.js
+++ api/server.js
@@ -1,18 +1,37 @@
 // npm modules
 var express = require('express'),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    cors = require('cors')
 
-      // express app
+// express app
 const app = express()
+
+// disable cors
+app.use(cors());
 
 // middleware sessions
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(function(error, req, res, next) {
+  console.log(req);
+  next();
+});
 
 // routes?
 app.get('/', (req, res) => {
+  console.log(req);
   res.send(`tried to login ${req.body.username} and pass ${req.body.password}`)
 })
+
+app.post('/problem/submit', (req, res) => {
+  const src = req.body.src;
+  const test = req.body.test;
+  console.log('src: ' + JSON.stringify(src));
+  console.log('test: ' + JSON.stringify(test));
+  console.log('received' + JSON.stringify(req.body));
+})
+
+// app.post('')
 
 
 // start express app
