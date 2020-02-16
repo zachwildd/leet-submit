d54575a555fe03bef6dffcf9325cbf9c0ccce899
Zach Wild <zacharyjwild@gmail.com>
2020-02-16 02:57:51 -0500

    update create db script

--- db/create_db.sql
+++ db/create_db.sql
@@ -10,3 +10,26 @@ CREATE TABLE `problem` (
   PRIMARY KEY (`problem_id`),
   UNIQUE KEY `problem_id_UNIQUE` (`problem_id`)
 );
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+);
+
+INSERT INTO problem 
+(
+`name`,
+`text`,
+`test`
+) 
+VALUES 
+(
+"Return 0",
+"Make this return 0",
+"const chai = require('chai'); const mocha = require('mocha'); const assert = chai.assert; const expect = chai.expect; describe('Response', function() { it('should construct the expected Response', function() { assert(1 === 2); }); });"
+);
+
+SELECT * FROM problem;
