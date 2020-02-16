755745ef0a21d78960c7c7434ed442adfb330cf5
Zach Wild <zacharyjwild@gmail.com>
2020-02-16 08:22:05 -0500

    add domain models for submission in api

---
+++ api/domain/submission.js
@@ -0,0 +1,10 @@
+class Submission {
+  constructor(problemId, userId, userCode, testCode) {
+    this.problemId = problemId;
+    this.userId = userId;
+    this.userCode = userCode;
+    this.testCode = testCode;
+  }
+}
+
+module.exports = Submission;
---
+++ api/domain/submissionResult.js
@@ -0,0 +1,7 @@
+class SubmissionResult {
+  constructor(testOutput) {
+    this.testOutput = testOutput;
+  }
+}
+
+module.exports = SubmissionResult;
