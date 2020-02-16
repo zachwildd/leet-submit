716de1d271863b47b99f9ab9b48805676566634d
Zach Wild <zacharyjwild@gmail.com>
2020-02-16 03:45:22 -0500

    refactor submit controller to use db

--- api/controllers/submit.controller.js
+++ api/controllers/submit.controller.js
@@ -1,12 +1,31 @@
+const models = require('../models/');
+
 const submitController = {}
 
-// post a new submit
+// post a new submission for a problem
 submitController.post = async (req, res) => {
-  const src = req.body.src;
-  const test = req.body.test;
-  console.log('src: ' + JSON.stringify(src));
-  console.log('test: ' + JSON.stringify(test));
-  res.send(req.body);
+  // get the problem id & user's code from request
+  const problemId = req.body.problemId;
+  const userCode = req.body.src;
+  console.log('problemId: ' + JSON.stringify(problemId));
+  console.log('userCode: ' + JSON.stringify(userCode));
+
+  // get the test code for the problem from the database
+  const testCode = models.problem.findOne({
+    where: {problem_id: problemId}
+  })
+      .then((problem) => {
+        // once we have the test code
+        res.send(problem.test);
+        // find a judge to test the submission
+        // get response from the judge
+        // return the results to the user
+        // do any other things with the results (keep record for user stats and so on)
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status('500').send();
+      });
 }
 
 module.exports = submitController
