a2254989cd79456b5c8644dda7a2652cf777e785
Don Isaac <donald.isaac@gmail.com>
2020-02-16 14:31:00 -0500

    add changes to create db script

--- db/create_db.sql
+++ db/create_db.sql
@@ -6,6 +6,7 @@ CREATE TABLE `problem` (
   `problem_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `text` text NOT NULL,
+  `default_code` text,
   `test` text,
   PRIMARY KEY (`problem_id`),
   UNIQUE KEY `problem_id_UNIQUE` (`problem_id`)
@@ -23,13 +24,15 @@ INSERT INTO problem
 (
 `name`,
 `text`,
+`default_text`,
 `test`
 ) 
 VALUES 
 (
 "Return 0",
 "Make this return 0",
-"const chai = require('chai'); const mocha = require('mocha'); const assert = chai.assert; const expect = chai.expect; describe('Response', function() { it('should construct the expected Response', function() { assert(1 === 2); }); });"
+"/**\n* Do not modify this classes existing methods; */\n class Solution {\n constructor() {}\n \n solution() {\n/** WRITE YOUR CODE IN THIS METHOD */\n return 0;\n}\n}\n module.exports = Solution;",
+"const chai = require('chai'); const mocha = require('mocha'); const Solution = require('./src'); const solution = new Solution(); const assert = chai.assert; const expect = chai.expect; describe('Response', function() { it('the solution should return 0', function() { assert(solution.solution() === 0); }); });"
 );
 
 SELECT * FROM problem;
